44b5eaa6521fe1665721a8acd65a448886daddd2
Jasir2772 <eshonbekovjasur9@gmail.com>
2026-09-09 14:35:34 +0500

    Create CosmeticParticle.java

---
+++ src/main/java/uz/cosmetics/plugin/CosmeticParticle.java
@@ -0,0 +1,43 @@
+package uz.cosmetics.plugin;
+
+import org.bukkit.Material;
+import org.bukkit.Particle;
+
+public enum CosmeticParticle {
+
+    NONE("O'chirish", Material.BARRIER, null),
+    HEART("Yurak", Material.RED_DYE, Particle.HEART),
+    FLAME("Olov", Material.BLAZE_POWDER, Particle.FLAME),
+    SNOW("Qor", Material.SNOWBALL, Particle.SNOWFLAKE),
+    PORTAL("Portal", Material.ENDER_PEARL, Particle.PORTAL),
+    CLOUD("Bulut", Material.WHITE_WOOL, Particle.CLOUD),
+    WITCH("Sehr", Material.GLOWSTONE_DUST, Particle.WITCH),
+    CRIT("Yulduzcha", Material.NETHER_STAR, Particle.CRIT),
+    NOTE("Musiqa notasi", Material.NOTE_BLOCK, Particle.NOTE),
+    SOUL("Jon", Material.SOUL_SAND, Particle.SOUL),
+    DRIP_LAVA("Lava tomchisi", Material.MAGMA_CREAM, Particle.DRIPPING_LAVA),
+    END_ROD("Yulduz chang", Material.END_ROD, Particle.END_ROD),
+    HAPPY_VILLAGER("Sparkle", Material.EMERALD, Particle.HAPPY_VILLAGER);
+
+    private final String displayName;
+    private final Material icon;
+    private final Particle particle;
+
+    CosmeticParticle(String displayName, Material icon, Particle particle) {
+        this.displayName = displayName;
+        this.icon = icon;
+        this.particle = particle;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Material getIcon() {
+        return icon;
+    }
+
+    public Particle getParticle() {
+        return particle;
+    }
+}
